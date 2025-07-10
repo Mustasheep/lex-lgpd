38189cbcea6ec44095de6e6f995740e0976f3193
Thiago de Assis <thiagoassis.escritorio@gmail.com>
2025-07-10 11:13:45 -0300

    feat: primeiro script de ingestão. versão 1

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -12,7 +12,7 @@ def busca_e_salva_lgpd():
     extrai o texto principal e o salva em um arquivo .txt
     """
     URL = "https://www.planalto.gov.br/ccivil_03/_ato2015-2018/2018/lei/l13709.htm"
-    RAW_DATA_PATH = os.path.join ("..", "data", "raw", "lgpd_raw_.txt")
+    RAW_DATA_PATH = os.path.join ("..", "data", "lgpd_raw_.txt")
     
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0',
@@ -27,21 +27,33 @@ def busca_e_salva_lgpd():
         logging.info("Analisando o HTML da página...")
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Buscando o id dentro do div 
-        texto_lei = soup.find(id='textoimpressao')
+        # Buscando o artigo dentro do div
+        lista_de_artigos = soup.select('p.Artigo')
 
-        if texto_lei:
-            logging.info("Texto com as leis encontrado, extraindo o texto...")
-            texto_puro = texto_lei.get_text(separator='\n', strip=True)
+        if lista_de_artigos:
+            logging.info(f"Encontrados {len(lista_de_artigos)} artigos.")
+            
+            # Iterando cada artigo e colocando em uma lista
+            texto_bruto = []
+            for i, artigo_tag in enumerate(lista_de_artigos):
+                texto_do_artigo = artigo_tag.get_text(strip=True)
+                texto_bruto.append(texto_do_artigo)
+            
+            # Convertendo lista para string para salvar com write
+            lgpd_raw = ' '.join(texto_bruto)
+            logging.info(f"Extração completa.")
 
-            # Criando os diretórios
+            # Criando o caminho para salvar
             os.makedirs(os.path.dirname(RAW_DATA_PATH), exist_ok=True)
 
             with open(RAW_DATA_PATH, 'w', encoding='utf-8') as f:
-                logging.info(f"O texto foi salvo com sucesso em: {RAW_DATA_PATH}")
-                f.write(texto_puro)
+                logging.info("Salvando em um arquivo de texto...")
+                f.write(lgpd_raw)
+
+            logging.info(f"Documento 'lgpd_raw.txt' salvo em: {RAW_DATA_PATH}")
+            
         else:
-            logging.error("Não foi possível encontrar o elemento chave para extração do texto.")
+            logging.error("Nenhum parágrafo com a classe 'Artigo' foi encontrado.")
     
     except requests.exceptions.RequestException as e:
         logging.critical(f"Erro ao fazer a requisição HTTP: {e}")
